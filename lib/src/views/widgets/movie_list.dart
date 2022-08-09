d617dc2250c32b7a1d7d5ee3f407373d4743f536
viktorvoltz <vctrchinyeaka@gmail.com>
2022-08-09 15:50:25 +0100

    movielist

---
+++ lib/src/views/widgets/movie_list.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../costants.dart';
+import '../../providers/providers.dart';
+
+class MovieList extends ConsumerWidget {
+  const MovieList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final moviesAsyncValue = ref.watch(moviesProvider);
+    return moviesAsyncValue.maybeWhen(
+      orElse: () => const Center(child: CircularProgressIndicator()),
+      data: (movies) => Center(
+        child: GridView.builder(
+          itemCount: movies.length,
+          itemBuilder: (BuildContext context, int index) {
+            final movie = movies[index];
+            return Card(
+              child: Container(
+                margin: const EdgeInsets.all(4),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                              "${EnvConstants.IMAGE_BASE_URL}${movie.posterPath}",
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text("${movie.title}")
+                  ],
+                ),
+              ),
+            );
+          },
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2),
+        ),
+      ),
+    );
+  }
+}
