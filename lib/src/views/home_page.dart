65cd214ac683861d8b4fe096fe54aac40ac27b47
viktorvoltz <vctrchinyeaka@gmail.com>
2022-08-09 15:21:15 +0100

    homepage

--- lib/src/views/home_page.dart
+++ lib/src/views/home_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({ Key? key }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
     return Container(
       
     );
