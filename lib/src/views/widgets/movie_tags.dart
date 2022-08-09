97111a3575f8b770a372d93c10a9754901dcdc0e
viktorvoltz <vctrchinyeaka@gmail.com>
2022-08-09 15:21:40 +0100

    movietag

---
+++ lib/src/views/widgets/movie_tags.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:inferno/src/providers/providers.dart';
+
+import '../../enums/movie_type.dart';
+
+class MovieTags extends ConsumerWidget {
+  const MovieTags({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final MovieType movieType = ref.watch(movieTypeProvider);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: MovieType.values
+      .map((type) => InkWell(
+        onTap: () => ref.read(movieTypeProvider.state)
+        .state = type,
+        child: Chip(
+          label: Text(
+            type.name,
+          ),
+          backgroundColor: type == movieType ? Colors.blue : null,
+        ),
+      ))
+      .toList(),
+    );
+  }
+}
