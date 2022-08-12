2827b641ae80e6838ae4f3afa15d8b3acf2360ca
viktorvoltz <vctrchinyeaka@gmail.com>
2022-08-12 09:07:26 +0100

    responsiveness

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'src/adaptive_screen.dart';
 import 'src/views/home_page.dart';
 
 
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: const Responsive(child: HomePage()),
     );
   }
 }
