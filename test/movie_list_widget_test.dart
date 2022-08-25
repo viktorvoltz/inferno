e5499388d66a7e3295c80e6e06beb663deb06917
viktorvoltz <vctrchinyeaka@gmail.com>
2022-08-25 19:46:04 +0100

    widget tests

---
+++ test/movie_list_widget_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:inferno/src/views/widgets/movie_list.dart';
+
+void main() {
+  testWidgets("movie list widget test", (tester) async {
+
+    await tester.pumpWidget(const MovieList());
+
+    expect(find.byType(Card), findsOneWidget);
+
+  });
+}
